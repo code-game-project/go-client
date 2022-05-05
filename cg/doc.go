1284e66a7edb970fb1bf8165f766cc0b807f1017
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-05-05 13:23:49 +0200

    Update package doc

--- cg/doc.go
+++ cg/doc.go
@@ -1,6 +1,6 @@
 /*
 Package cg implements common client logic for connecting with a CodeGame server and handling events.
 
-This version implements the CodeGame specification as layed out in https://code-game-project.github.io/docs/specification-v1.html.
+This version implements the CodeGame specification v0.0.5 as layed out in https://code-game-project.github.io/docs/protocol-specification.html.
 */
 package cg
